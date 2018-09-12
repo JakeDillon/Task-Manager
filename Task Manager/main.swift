b064faaf4784aaf11bd0309a09506112ed8bc3df
Jake Dillon <jakedillon@macbook-pro-60.local>
2018-09-12 10:10:57 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Jake Dillon on 9/12/18.
+//  Copyright © 2018 Jake Dillon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
